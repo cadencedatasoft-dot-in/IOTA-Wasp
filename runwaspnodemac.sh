c2e96884d8308731e555452011a7003a3598e7ef
Anand Bhalve <banand@cadencedatasoft.com>
2022-05-05 22:09:02 +0530

    Mac support added

---
+++ runwaspnodemac.sh
@@ -0,0 +1,25 @@
+brew install git
+brew install jq
+
+# You may change the branch here
+git clone --branch v0.2.5 https://github.com/iotaledger/wasp.git
+cd wasp
+go get github.com/lucas-clemente/quic-go@v0.26.0
+echo "Building wasp..., this may take some time"
+make &>/dev/null
+./wasp-cli init
+./wasp-cli set goshimmer.api https://api.goshimmer.sc.iota.org
+./wasp-cli set wasp.0.api 127.0.0.1:9090
+./wasp-cli set wasp.0.nanomsg 127.0.0.1:5550
+./wasp-cli set wasp.0.peering 127.0.0.1:4000
+
+jq '.nodeconn.address = "goshimmer.sc.iota.org:5000"' config.json > tmp.$$.json && mv tmp.$$.json config.json
+
+# Start wasp node in new tab
+cwd=$(pwd)
+osascript -e 'tell app "Terminal" to do script "cd '$cwd'; sudo ./wasp"'
+
+# Wait and request for funds 
+sleep 30s
+./wasp-cli request-funds
+open http://localhost:7000
